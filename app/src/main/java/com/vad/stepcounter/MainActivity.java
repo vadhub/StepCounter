daf8f140bf252a5e38248dc4866c780da1f5d598
Vadim <gabderahmanov99@gmail.com>
2021-06-26 23:50:15 +0300

    add permission check for activity recognition

--- app/src/main/java/com/vad/stepcounter/MainActivity.java
+++ app/src/main/java/com/vad/stepcounter/MainActivity.java
@@ -1,19 +1,61 @@
 package com.vad.stepcounter;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.vad.stepcounter.fragments.stepscounted.SensorBackgroundCounter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_PHYSICAL_ACTIVITY_CODE = 123334;
+
+    private void checkPermission(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION)== PackageManager.PERMISSION_DENIED){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, REQUEST_PHYSICAL_ACTIVITY_CODE);
+            }
+        }else{
+            startServiceForeground();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        checkPermission();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==REQUEST_PHYSICAL_ACTIVITY_CODE){
+            if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                startServiceForeground();
+                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void startServiceForeground(){
         Intent intent = new Intent(MainActivity.this, SensorBackgroundCounter.class);
         startService(intent);
     }
