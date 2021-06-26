95a38464b7744d6674d0bd7f80be11953184f4ef
Vadim <gabderahmanov99@gmail.com>
2021-06-26 22:52:00 +0300

    Initial commit

---
+++ app/src/main/java/com/vad/stepcounter/fragments/SteperFragment.java
@@ -0,0 +1,22 @@
+package com.vad.stepcounter.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.vad.stepcounter.R;
+
+public class SteperFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_steper, container, false);
+
+        return v;
+    }
+}
---
+++ app/src/main/java/com/vad/stepcounter/utils/UtilsMath.java
@@ -0,0 +1,9 @@
+package com.vad.stepcounter.utils;
+
+public class UtilsMath {
+
+    public static float calories(int step, float weight, int height, int timeMin){
+        return step;
+    }
+
+}
